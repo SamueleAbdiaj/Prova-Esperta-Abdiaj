6d4aee7de6cc7676439635647aa18711051ad697
Samuele Abdiaj <s.abdiaj@donboscosandona.it>
2025-06-09 12:30:18 +0200

    push di prova

---
+++ prova-esperta-abdiaj/app/page.js
@@ -0,0 +1,12 @@
+// app/page.js
+import NavBar from './components/NavBar'; // Corrected path
+
+export default function Home() {
+  return (
+    <> 
+      <div className=''>
+        <NavBar />
+      </div>
+    </>
+  );
+}
